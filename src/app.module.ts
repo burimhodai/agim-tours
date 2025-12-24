465acc3bcfba77a0b9ca8cb9208b97b5f2403811
burimhodai <burimhod@gmail.com>
2025-12-24 15:30:58 +0100

    aaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import configuration from './config/configuration';
-import { validationSchema } from './config/validation.schema';
 
 @Module({
   imports: [
@@ -12,7 +10,6 @@ import { validationSchema } from './config/validation.schema';
     ConfigModule.forRoot({
       isGlobal: true, // Makes ConfigService available everywhere
       load: [configuration], // Load custom configuration
-      validationSchema, // Validate environment variables
       validationOptions: {
         allowUnknown: true, // Allow unknown env variables
         abortEarly: false, // Show all validation errors
@@ -22,7 +19,6 @@ import { validationSchema } from './config/validation.schema';
     }),
 
     // Database Module
-    DatabaseModule,
 
     // Feature Modules (will be added here)
     // AuthModule,
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,8 +5,5 @@ export const getDatabaseConfig = (
   configService: ConfigService,
 ): MongooseModuleOptions => ({
   uri: configService.get<string>('database.uri'),
-  autoIndex: true, // Build indexes on startup (disable in production if using manual indexing)
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
 });
